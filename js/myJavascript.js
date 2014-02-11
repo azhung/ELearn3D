4630dd12e1307b7b59430624ebff8cc47ecc9a37
azhung <azhung.it@gmail.com>
2014-02-11 10:56:26 +0700

    This file store a customer javascript

---
+++ js/myJavascript.js
@@ -0,0 +1,27 @@
+var myJs = {
+	
+	addEvent : (function () {
+		if (typeof addEventListener !== "undefined") {
+			return function (obj, evt, fn) {
+				obj.addEventListener(evt, fn, false);
+			};
+		} else {
+			return function (obj, evt, fn) {
+				obj.attachEvent("on" + evt, fn);
+			};
+		}
+	}()),
+	
+	getTarget : (function() {
+		if (typeof addEventListener !== "undefined") {
+			return function(event) {
+				return event.target;
+			};
+		} else {
+			return function(event) {
+				return event.srcElement;
+			}
+		}
+	}())
+	
+}
